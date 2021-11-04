d41741abe1933d768517f754cbf7fc5f72be1836
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-04 11:19:03 -0700

    add another p, to align the small p in the last component.

--- src/App.js
+++ src/App.js
@@ -33,7 +33,9 @@ function App() {
         <h1>Your team is busy.</h1>
           <div className="shape__child--flex">
             <p>Increase productivity with react reusable components</p>
-            <p>Increase morale</p>
+            <p>
+              <p>Increase morale</p>
+            </p>
             <p>Getting shit done - now that's a great feeling!</p>
           </div>
       </Shape>
