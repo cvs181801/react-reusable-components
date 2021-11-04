5ec52e51ee41cda8c5fa672c452e0114ca2dcae5
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-04 11:08:12 -0700

    add some more content to practice passing in children

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
       <Shape>
           <div className="shape__child">
-            <p>Your app will be far easier to maintain with React reusaable components!</p>
+            <p>Your app will be far easier to maintain with React reusable components!</p>
           </div>
       </Shape>
 
@@ -19,6 +19,22 @@ function App() {
         <h2>Scalability is key to your organization.</h2>
           <div className="shape__child">
             <p>Don't let poor design eat up your time</p>
+          </div>
+      </Shape>
+
+      <Shape>
+          <div className="shape__child--flex">
+            <p>React provides tools for increased cybersecurity</p>
+            <p>Don't wait until it's too late</p>
+          </div>
+      </Shape>
+
+      <Shape>
+        <h1>Your team is busy.</h1>
+          <div className="shape__child--flex">
+            <p>Increase productivity with react reusable components</p>
+            <p>increase scalability</p>
+            <p>Getting shit done - now that's a great feeling!</p>
           </div>
       </Shape>
 
